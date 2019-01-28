4b18088927f439b9a309d585c6fcf96d9e76afb0
Arun Barua <arunsworld@gmail.com>
2019-01-29 00:41:44 +0530

    Refactored dataio to it's own package

---
+++ dataio/dataio.go
@@ -0,0 +1,134 @@
+package dataio
+
+import (
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"io"
+)
+
+// RecordProcessor is the function which is called for every record in the CSV
+type RecordProcessor func(record []string, header bool)
+
+// ErrorRecordProcessor is the function which is called for an error row during CSV processing
+type ErrorRecordProcessor func(row int, e error)
+
+// CSVSpec specifies the CSV parser
+type CSVSpec struct {
+	Comma rune
+}
+
+// ParseCSV parses the reader as CSV and calls the RecordProcessor for each record
+func ParseCSV(r io.Reader, spec CSVSpec, processor RecordProcessor, errorProcessor ErrorRecordProcessor) {
+	csvReader := csv.NewReader(r)
+	if spec.Comma != 0 {
+		csvReader.Comma = spec.Comma
+	}
+	header := true
+	rowCounter := 0
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if !header {
+			rowCounter++
+		}
+		if err != nil {
+			errorProcessor(rowCounter-1, err)
+			continue
+		}
+		processor(record, header)
+		header = false
+	}
+}
+
+// LineProcessor is the function called for each line while Parsing lines
+type LineProcessor func(line string)
+
+// ParseLinesAsStrings parses lines from a reader
+func ParseLinesAsStrings(r io.Reader, processor LineProcessor) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		processor(scanner.Text())
+	}
+}
+
+// LineProcessorWithAbort is the function called for each line that supports returning an Abort error
+type LineProcessorWithAbort func(line string) error
+
+// ParseLinesAsStringsWithAbort parses line from a reader with option to abort
+func ParseLinesAsStringsWithAbort(r io.Reader, processor LineProcessorWithAbort) error {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		if err := processor(scanner.Text()); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// BinaryLineProcessor is the function called for each line while Parsing lines
+type BinaryLineProcessor func(line []byte)
+
+// ParseLinesAsBytes parses lines from a reader
+func ParseLinesAsBytes(r io.Reader, processor BinaryLineProcessor) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		processor(scanner.Bytes())
+	}
+}
+
+// CRFixer fixes CR to \r\n
+type CRFixer struct {
+	r io.Reader
+	q []byte
+}
+
+func (crf *CRFixer) Read(p []byte) (int, error) {
+	if len(crf.q) < len(p)/2 {
+		crf.q = make([]byte, len(p)/2)
+	}
+	n, err := crf.r.Read(crf.q)
+	counter := 0
+	ignoreNL := false
+	for i := 0; i < n; i++ {
+		ch := crf.q[i]
+		if ignoreNL && ch == '\n' {
+			ignoreNL = false
+			continue
+		}
+		p[counter] = ch
+		if ch == '\r' {
+			counter++
+			p[counter] = '\n'
+			ignoreNL = true
+		}
+		counter++
+	}
+	return counter, err
+}
+
+// CRNewLineFixer converts \r to \n
+func CRNewLineFixer(in io.Reader) *CRFixer {
+	return &CRFixer{r: in}
+}
+
+// WriteCSV writes to a CSV writer the records
+func WriteCSV(spec CSVSpec, records [][]string, w io.Writer) error {
+	csvWriter := csv.NewWriter(w)
+	if spec.Comma != 0 {
+		csvWriter.Comma = spec.Comma
+	}
+	for _, record := range records {
+		if err := csvWriter.Write(record); err != nil {
+			return errors.New("error writing record to csv: " + err.Error())
+		}
+	}
+	csvWriter.Flush()
+
+	if err := csvWriter.Error(); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ dataio/dataio_test.go
@@ -0,0 +1,387 @@
+package dataio
+
+import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestCSVParser(t *testing.T) {
+	data := `first_name,last_name,username
+"Rob","Pike",rob
+bad record,test
+Ken,Thompson,ken
+second bad record,""test""
+Arun,"Barua",abarua
+`
+
+	spec := CSVSpec{}
+
+	t.Run("Header", func(t *testing.T) {
+		expected := []string{"first_name", "last_name", "username"}
+		headerFound := false
+		r := strings.NewReader(data)
+		ParseCSV(r, spec, func(record []string, header bool) {
+			if !header {
+				return
+			}
+			headerFound = true
+			if len(record) != len(expected) {
+				t.Errorf("Header length not as expected: %d instead of %d.", len(record), len(expected))
+				return
+			}
+			for i, v := range expected {
+				if record[i] != v {
+					t.Errorf("Unexpected value in header for column %d. %s instead of %s.", i, record[i], v)
+					return
+				}
+			}
+		}, func(row int, e error) {})
+		if !headerFound {
+			t.Error("Header not found.")
+		}
+	})
+
+	t.Run("Rows", func(t *testing.T) {
+		expected := [][]string{
+			{"Rob", "Pike", "rob"},
+			{"Ken", "Thompson", "ken"},
+			{"Arun", "Barua", "abarua"},
+		}
+		found := make([]bool, len(expected))
+		counter := 0
+		r := strings.NewReader(data)
+		ParseCSV(r, spec, func(record []string, header bool) {
+			if header {
+				return
+			}
+			if counter == len(expected) {
+				t.Error("Found more rows than expected!")
+				return
+			}
+			exp := expected[counter]
+			if len(exp) != len(record) {
+				t.Errorf("For row %d expected: %d, got %d.", counter, len(exp), len(record))
+				return
+			}
+			for i, v := range exp {
+				if record[i] != v {
+					t.Errorf("For row %d and column %d, expected: %s instead of %s.", counter+1, i+1, v, record[i])
+				}
+			}
+			found[counter] = true
+			counter++
+		}, func(row int, e error) {})
+		for i, v := range found {
+			if !v {
+				t.Errorf("Did not find row %d.", i)
+			}
+		}
+	})
+
+	t.Run("Errors", func(t *testing.T) {
+		r := strings.NewReader(data)
+		errorFound := []bool{false, false}
+		ParseCSV(r, spec, func(record []string, header bool) {}, func(row int, e error) {
+			parseError, ok := e.(*csv.ParseError)
+			if !ok {
+				t.Errorf("Expected parseError, instead found: %T for row: %d", e, row)
+				return
+			}
+			if row == 1 {
+				if parseError.Err != csv.ErrFieldCount {
+					t.Error("Expected ErrFieldCount instead got: ", parseError.Err)
+				}
+				errorFound[0] = true
+			}
+			if row == 3 {
+				if parseError.Err != csv.ErrQuote {
+					t.Error("Expected ErrQuote instead got: ", parseError.Err)
+				}
+				errorFound[1] = true
+			}
+		})
+		for i, e := range errorFound {
+			if e {
+				continue
+			}
+			t.Errorf("Error %d expected but not found.", i+1)
+		}
+	})
+
+	t.Run("Delimiter", func(t *testing.T) {
+		data := `A|B|C
+D|E|F`
+		expected := [][]string{
+			{"A", "B", "C"},
+			{"D", "E", "F"},
+		}
+		found := make([]bool, len(expected))
+		counter := 0
+		r := strings.NewReader(data)
+		spec := CSVSpec{Comma: '|'}
+		ParseCSV(r, spec, func(record []string, header bool) {
+			if counter == len(expected) {
+				t.Error("Found more rows than expected!")
+				return
+			}
+			exp := expected[counter]
+			if len(exp) != len(record) {
+				t.Errorf("For row %d expected: %d, got %d.", counter, len(exp), len(record))
+				return
+			}
+			for i, v := range exp {
+				if record[i] != v {
+					t.Errorf("For row %d and column %d, expected: %s instead of %s.", counter+1, i+1, v, record[i])
+				}
+			}
+			found[counter] = true
+			counter++
+		}, func(row int, e error) {})
+		for i, v := range found {
+			if !v {
+				t.Errorf("Did not find row %d.", i)
+			}
+		}
+	})
+}
+
+func TestLineParser(t *testing.T) {
+	data := `line 1
+line 2
+line 3`
+
+	t.Run("Rows As Strings", func(t *testing.T) {
+		r := strings.NewReader(data)
+
+		expected := []string{
+			"line 1",
+			"line 2",
+			"line 3",
+		}
+		found := make([]bool, len(expected))
+		counter := 0
+		ParseLinesAsStrings(r, func(line string) {
+			if counter == len(expected) {
+				t.Error("Found an un-expected line!", line)
+				return
+			}
+			if line != expected[counter] {
+				t.Errorf("Expected %s, found %s.", expected[counter], line)
+			}
+			found[counter] = true
+			counter++
+		})
+		for i, f := range found {
+			if !f {
+				t.Errorf("Line %d not found.", i+1)
+			}
+		}
+	})
+
+	t.Run("Rows As Byte Arrays", func(t *testing.T) {
+		r := strings.NewReader(data)
+
+		expected := [][]byte{
+			[]byte("line 1"),
+			[]byte("line 2"),
+			[]byte("line 3"),
+		}
+		found := make([]bool, len(expected))
+		counter := 0
+		ParseLinesAsBytes(r, func(line []byte) {
+			if counter == len(expected) {
+				t.Error("Found an expected line!", line)
+				return
+			}
+			if bytes.Compare(expected[counter], line) != 0 {
+				t.Errorf("Expected %v, found %v.", expected[counter], line)
+			}
+			found[counter] = true
+			counter++
+		})
+		for i, f := range found {
+			if !f {
+				t.Errorf("Line %d not found.", i+1)
+			}
+		}
+	})
+
+}
+
+func TestLineParserWithAbort(t *testing.T) {
+	data := `line 1
+line 2
+line 3
+line 4`
+
+	t.Run("With Strings and Abort", func(t *testing.T) {
+		r := strings.NewReader(data)
+
+		expected := []string{
+			"line 1",
+			"line 2",
+		}
+		found := make([]bool, len(expected))
+		counter := 0
+		err := ParseLinesAsStringsWithAbort(r, func(line string) error {
+			if counter == 2 {
+				return errors.New("abort from routine")
+			}
+			if counter == len(expected) {
+				t.Error("Found an un-expected line!", line)
+				return nil
+			}
+			if line != expected[counter] {
+				t.Errorf("Expected %s, found %s.", expected[counter], line)
+			}
+			found[counter] = true
+			counter++
+			return nil
+		})
+		if err == nil {
+			t.Error("Expecting to receive an error but did not get one.")
+			return
+		}
+		if err.Error() != "abort from routine" {
+			t.Error("Expecting to get error - abort from routine - but didn't get")
+			return
+		}
+		for i, f := range found {
+			if !f {
+				t.Errorf("Line %d not found.", i+1)
+			}
+		}
+	})
+
+	t.Run("With Strings without Abort", func(t *testing.T) {
+		r := strings.NewReader(data)
+
+		expected := []string{
+			"line 1",
+			"line 2",
+			"line 3",
+			"line 4",
+		}
+		found := make([]bool, len(expected))
+		counter := 0
+		err := ParseLinesAsStringsWithAbort(r, func(line string) error {
+			if counter == len(expected) {
+				t.Error("Found an un-expected line!", line)
+				return nil
+			}
+			if line != expected[counter] {
+				t.Errorf("Expected %s, found %s.", expected[counter], line)
+			}
+			found[counter] = true
+			counter++
+			return nil
+		})
+		if err != nil {
+			t.Error("Was not expecting an error but got: " + err.Error())
+			return
+		}
+		for i, f := range found {
+			if !f {
+				t.Errorf("Line %d not found.", i+1)
+			}
+		}
+	})
+
+}
+
+func TestFixCROnlyNewLine(t *testing.T) {
+
+	t.Run("Simple Happy Path", func(t *testing.T) {
+		data := "this is a test.\rAnother line here.\r"
+		expected := "this is a test.\r\nAnother line here.\r\n"
+
+		r := strings.NewReader(data)
+		newR := CRNewLineFixer(r)
+
+		result, err := ioutil.ReadAll(newR)
+		if err != nil {
+			t.Error("Encountered error: ", err)
+			return
+		}
+
+		if string(result) != expected {
+			t.Errorf("Expected %s, got %s.", expected, string(result))
+		}
+	})
+
+	t.Run("Avoid Doubles", func(t *testing.T) {
+		data := "this is a test.\r\nAnother line here.\r\n"
+		expected := "this is a test.\r\nAnother line here.\r\n"
+
+		r := strings.NewReader(data)
+		newR := CRNewLineFixer(r)
+
+		result, err := ioutil.ReadAll(newR)
+		if err != nil {
+			t.Error("Encountered error: ", err)
+			return
+		}
+
+		if string(result) != expected {
+			t.Errorf("Expected %s, got %s.", expected, string(result))
+		}
+	})
+
+	t.Run("All CR", func(t *testing.T) {
+		data := "\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r"
+		data = data + data + data + data + data + data + data + data
+		data = data + data + data + data + data + data + data + data
+		data = data + data + data + data + data + data + data + data
+		data = data + data + data + data + data + data + data + data
+		data = data + data + data + data + data + data + data + data
+		expected := "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n"
+		expected = expected + expected + expected + expected + expected + expected + expected + expected
+		expected = expected + expected + expected + expected + expected + expected + expected + expected
+		expected = expected + expected + expected + expected + expected + expected + expected + expected
+		expected = expected + expected + expected + expected + expected + expected + expected + expected
+		expected = expected + expected + expected + expected + expected + expected + expected + expected
+
+		r := strings.NewReader(data)
+		newR := CRNewLineFixer(r)
+
+		result, err := ioutil.ReadAll(newR)
+		if err != nil {
+			t.Error("Encountered error: ", err)
+			return
+		}
+
+		if string(result) != expected {
+			t.Error("Strings did not match.")
+		}
+	})
+}
+
+func TestCSVWriter(t *testing.T) {
+	records := [][]string{
+		{"first_name", "last_name", "username"},
+		{"Rob", "Pike", "rob"},
+		{"Ken", "Thompson", "ken"},
+	}
+
+	output := bytes.Buffer{}
+	spec := CSVSpec{
+		Comma: '\t',
+	}
+
+	if err := WriteCSV(spec, records, &output); err != nil {
+		t.Error("Could not write CSV to file: ", err)
+		return
+	}
+
+	expected := "first_name\tlast_name\tusername\n" +
+		"Rob\tPike\trob\n" +
+		"Ken\tThompson\tken\n"
+
+	if output.String() != expected {
+		t.Errorf("Output did not match the expected. %s instead of %s", output.String(), expected)
+	}
+}
